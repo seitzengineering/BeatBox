289a1f33226c64e2598de7b99c16853dad274631
seitzengineering <kseitz@okstate.edu>
2016-03-25 22:51:49 -0500

    Initial commit

---
+++ app/src/main/java/com/seitzsoftwaresolutions/android/beatbox/Sound.java
@@ -0,0 +1,33 @@
+package com.seitzsoftwaresolutions.android.beatbox;
+
+/**
+ * Created by Kevin on 2/28/2016.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+    private Integer mSoundId;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] componets = assetPath.split("/");
+        String filename = componets[componets.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+}
